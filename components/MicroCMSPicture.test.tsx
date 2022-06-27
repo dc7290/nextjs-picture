c470e29ee328a0b09fb45ac37e4650c24880b234
dc7290 <dhkh.cba0927@gmail.com>
2022-06-28 05:54:57 +0900

    テスト準備ファイルを作成

---
+++ components/MicroCMSPicture.test.tsx
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom'
+
+import { render, screen } from '@testing-library/react'
+
+import MicroCMSPicture from './MicroCMSPicture'
+
+describe('MicroCMSPicture', () => {
+  it.todo('デフォルトの動作')
+  it.todo('アートディレクションの動作')
+})
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,18 @@
-const nextJest = require("next/jest");
+const nextJest = require('next/jest')
 
-// Providing the path to your Next.js app which will enable loading next.config.js and .env files
-const createJestConfig = nextJest({ dir: "./" });
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
 
-// Any custom config you want to pass to Jest
+// Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-};
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+}
 
-// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
-module.exports = createJestConfig(customJestConfig);
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
