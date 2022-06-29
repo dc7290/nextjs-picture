37f0edaddd42405ab83d4ea5c1ab9d712876174c
dc7290 <dhkh.cba0927@gmail.com>
2022-06-29 15:57:31 +0900

    WIP: getSources

--- components/MicroCMSPicture.test.tsx
+++ components/MicroCMSPicture.test.tsx
@@ -1,10 +1,128 @@
 import '@testing-library/jest-dom'
 
 import { render, screen } from '@testing-library/react'
+import { NextConfig } from 'next'
+import { IMAGECONFIG_DEFAULT } from '../utils/constants'
 
-import MicroCMSPicture from './MicroCMSPicture'
+import MicroCMSPicture, { getSources } from './MicroCMSPicture'
+
+const createConfig = (customConfig?: NextConfig['images']) => {
+  return Object.assign(IMAGECONFIG_DEFAULT, customConfig)
+}
 
 describe('MicroCMSPicture', () => {
+  describe('getSources', () => {
+    it('デフォルトの動作', () => {
+      const sources = getSources({
+        config: createConfig({
+          deviceSizes: [640, 1200],
+        }),
+        src: 'https://remote-image/image.png',
+      })
+
+      expect(sources).toStrictEqual([
+        {
+          height: undefined,
+          srcSet:
+            'https://remote-image/image.png?fit=max&w=640&q=75&fm=avif 640w, https://remote-image/image.png?fit=max&w=1200&q=75&fm=avif 1200w',
+          type: 'image/avif',
+          width: undefined,
+        },
+        {
+          height: undefined,
+          srcSet:
+            'https://remote-image/image.png?fit=max&w=640&q=75&fm=webp 640w, https://remote-image/image.png?fit=max&w=1200&q=75&fm=webp 1200w',
+          type: 'image/webp',
+          width: undefined,
+        },
+      ])
+    })
+    it('アートディレクションの動作', () => {
+      const sources = getSources({
+        config: createConfig({
+          deviceSizes: [640, 1200],
+        }),
+        artDirevtives: [
+          {
+            src: 'https://remote-image/image_lg.png',
+            media: '(min-width: 1024px)',
+            width: 1920,
+            height: 1200,
+          },
+          {
+            src: 'https://remote-image/image_md.png',
+            media: '(min-width: 768px)',
+            width: 1920,
+            height: 800,
+          },
+        ],
+        src: 'https://remote-image/image.png',
+      })
+      expect(sources).toStrictEqual([
+        {
+          srcSet:
+            'https://remote-image/image_lg.png?fit=max&w=640&q=75&fm=avif 640w, https://remote-image/image_lg.png?fit=max&w=1200&q=75&fm=avif 1200w',
+          type: 'image/avif',
+          media: '(min-width: 1024px)',
+          width: 1920,
+          height: 1200,
+        },
+        {
+          srcSet:
+            'https://remote-image/image_lg.png?fit=max&w=640&q=75&fm=webp 640w, https://remote-image/image_lg.png?fit=max&w=1200&q=75&fm=webp 1200w',
+          type: 'image/webp',
+          media: '(min-width: 1024px)',
+          width: 1920,
+          height: 1200,
+        },
+        {
+          srcSet:
+            'https://remote-image/image_lg.png?fit=max&w=640&q=75 640w, https://remote-image/image_lg.png?fit=max&w=1200&q=75 1200w',
+          media: '(min-width: 1024px)',
+          width: 1920,
+          height: 1200,
+        },
+        {
+          srcSet:
+            'https://remote-image/image_md.png?fit=max&w=640&q=75&fm=avif 640w, https://remote-image/image_md.png?fit=max&w=1200&q=75&fm=avif 1200w',
+          type: 'image/avif',
+          media: '(min-width: 768px)',
+          width: 1920,
+          height: 800,
+        },
+        {
+          srcSet:
+            'https://remote-image/image_md.png?fit=max&w=640&q=75&fm=webp 640w, https://remote-image/image_md.png?fit=max&w=1200&q=75&fm=webp 1200w',
+          type: 'image/webp',
+          media: '(min-width: 768px)',
+          width: 1920,
+          height: 800,
+        },
+        {
+          srcSet:
+            'https://remote-image/image_md.png?fit=max&w=640&q=75 640w, https://remote-image/image_md.png?fit=max&w=1200&q=75 1200w',
+          media: '(min-width: 768px)',
+          width: 1920,
+          height: 800,
+        },
+        // {
+        //   height: undefined,
+        //   srcSet:
+        //     'https://remote-image/image.png?fit=max&w=640&q=75&fm=avif 640w, https://remote-image/image.png?fit=max&w=1200&q=75&fm=avif 1200w',
+        //   type: 'image/avif',
+        //   width: undefined,
+        // },
+        // {
+        //   height: undefined,
+        //   srcSet:
+        //     'https://remote-image/image.png?fit=max&w=640&q=75&fm=webp 640w, https://remote-image/image.png?fit=max&w=1200&q=75&fm=webp 1200w',
+        //   type: 'image/webp',
+        //   width: undefined,
+        // },
+      ])
+    })
+  })
+
   it.todo('デフォルトの動作')
   it.todo('アートディレクションの動作')
 })
--- components/MicroCMSPicture.tsx
+++ components/MicroCMSPicture.tsx
@@ -1,21 +1,134 @@
-import Image, { ImageProps } from 'next/image'
-import { ImageConfigContext } from 'next/dist/shared/lib/image-config-context'
-import { useContext } from 'react'
+import { ImageConfigComplete } from 'next/dist/shared/lib/image-config'
+import Image, { ImageLoader, ImageLoaderProps, ImageProps } from 'next/image'
+import { DetailedHTMLProps, SourceHTMLAttributes } from 'react'
+import useImageConfig from '../hooks/useImageConfig'
 
 type ArtDirective = {
+  src: string
   media: string
+  width?: number
+  height?: number
 }
 
-type Props = {
-  artDirevtives?: ArtDirective[]
-} & Omit<ImageProps, 'blurDataURL' | 'loader'>
+const FORMATS = ['image/avif', 'image/webp']
 
-const MicroCMSPicture = ({ src, ...props }: Props) => {
-  const nextImageConfig = useContext(ImageConfigContext)
+type GetSourcesArgs = {
+  config: ImageConfigComplete
+  src: string
+  width?: number
+  height?: number
+  quality?: number
+  formats?: string[]
+  artDirevtives?: ArtDirective[]
+}
+type GetSourcesResult = DetailedHTMLProps<
+  SourceHTMLAttributes<HTMLSourceElement>,
+  HTMLSourceElement
+>[]
+export const getSources = ({
+  config,
+  src,
+  width,
+  height,
+  quality = 75,
+  formats = FORMATS,
+  artDirevtives,
+}: GetSourcesArgs): GetSourcesResult => {
+  const getFotmatParam = (format: string) => format.replace(/^image\//, '')
+  const getSrcSet = (src: string, format?: string) =>
+    config.deviceSizes
+      .map(
+        (deviceSize) =>
+          `${loader({
+            src,
+            width: deviceSize,
+            quality,
+            format: format !== undefined ? getFotmatParam(format) : undefined,
+          })} ${deviceSize}w`
+      )
+      .join(', ')
+
+  if (artDirevtives !== undefined) {
+    if (!Array.isArray(artDirevtives)) {
+      throw Error('`artDirevtives`には配列を指定してください。')
+    }
+
+    return artDirevtives
+      .map(({ src, media, width, height }) => [
+        ...formats.map((format) => ({
+          srcSet: getSrcSet(src, format),
+          type: format,
+          media,
+          width,
+          height,
+        })),
+        { srcSet: getSrcSet(src), media, width, height },
+      ])
+      .flat()
+  }
+
+  return formats.map((format) => ({
+    srcSet: getSrcSet(src, format),
+    type: format,
+    width,
+    height,
+  }))
+}
+
+const normalizeSrc = (src: string): string => {
+  return src[0] === '/' ? src.slice(1) : src
+}
+
+const loader = ({
+  src,
+  width,
+  quality,
+  format,
+}: ImageLoaderProps & { format?: string }) => {
+  const url = new URL(normalizeSrc(src))
+  const params = url.searchParams
+
+  params.set('fit', params.get('fit') || 'max')
+  params.set('w', params.get('w') || width.toString())
+
+  if (quality) {
+    params.set('q', quality.toString())
+  }
+
+  if (format) {
+    params.set('fm', format)
+  }
+
+  return url.href
+}
+
+type Props = Omit<ImageProps, 'blurDataURL' | 'loader'> & {
+  src: string
+  artDirevtives?: ArtDirective[]
+}
+
+const MicroCMSPicture = ({
+  src,
+  width,
+  height,
+  quality,
+  artDirevtives,
+  ...props
+}: Props) => {
+  const nextImageConfig = useImageConfig()
+
+  const sources = getSources({
+    src,
+    width: Number(width),
+    height: Number(height),
+    quality: Number(quality),
+    config: nextImageConfig,
+    artDirevtives,
+  })
 
   return (
     <picture>
-      <Image src={src} {...props} />
+      <Image src={src} loader={loader} layout="raw" {...props} />
     </picture>
   )
 }
